027197fcb6b1c550b3fe28bb30de7bcc522fad1e
Cindy Andrade <cindya3939@gmail.com>
2024-07-18 14:38:40 -0700

    Temporarily implement Supabase SQL Tutorial

--- App.js
+++ App.js
@@ -1,19 +1,41 @@
-import "react-native-gesture-handler";
-import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from "@react-navigation/native";
-import HomeScreen from "./screens/HomeScreen";
-import ChatScreen from "./screens/ChatScreen";
 
-const Stack = createStackNavigator();
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList, SafeAreaView } from 'react-native';
+import { supabase } from './utils/supabase';
 
 export default function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const getTodos = async () => {
+      try {
+        const { data: todos, error } = await supabase.from('todos').select();
+
+        if (error) {
+          console.error('Error fetching todos:', error.message);
+          return;
+        }
+
+        if (todos && todos.length > 0) {
+          setTodos(todos);
+        }
+      } catch (error) {
+        console.error('Error fetching todos:', error.message);
+      }
+    };
+
+    getTodos();
+  }, []);
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
-        <Stack.Screen name="ChatScreen" component={ChatScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-    // <HomeScreen />
+    <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Todo List</Text>
+      <FlatList
+        data={todos}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => <Text key={item.id}>{item.title}</Text>}
+      />
+    </SafeAreaView>
   );
-}
+};
+
